b3d6046156d050f7ccb831f330a47be46fa910b4
jovana vieira miranda <jovanavieira802@gmail>
2026-09-08 19:29:29 -0300

    feat: estrutura de pastas, componentes e rotas da etapa 4

---
+++ src/app/shared/header/header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  imports: [RouterLink],
+  selector: 'app-header',
+  styleUrl: './header.css',
+  templateUrl: './header.html',
+})
+export class Header {}
